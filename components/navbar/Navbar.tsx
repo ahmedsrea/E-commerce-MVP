7f61b2378c2d6d2d6007eec347630d05a0f0b2a4
ahmedsrea <ahmedfast13@gmail.com>
2024-01-07 18:23:53 -0800

    feat: create navbar

---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,95 @@
+import Link from "next/link";
+import { IoMenuSharp } from "react-icons/io5";
+import Container from "../Container";
+import { MdExpandMore } from "react-icons/md";
+import ReactCountryFlag from "react-country-flag";
+
+export default function Navbar() {
+  return (
+    <nav className="py-4 border-b border-[#E0E0E0]">
+      <Container>
+        <div className="flex items-center justify-between">
+          <ul className="flex items-center gap-9">
+            <li>
+              <Link
+                href="/"
+                className="flex gap-1 items-center text-[#1C1C1C] font-medium"
+              >
+                <IoMenuSharp size="22" /> All category
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/"
+                className="flex gap-1 items-center text-[#1C1C1C] font-medium"
+              >
+                Hot offers
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/"
+                className="flex gap-1 items-center text-[#1C1C1C] font-medium"
+              >
+                Gift boxes
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/"
+                className="flex gap-1 items-center text-[#1C1C1C] font-medium"
+              >
+                Projects
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/"
+                className="flex gap-1 items-center text-[#1C1C1C] font-medium"
+              >
+                Menu item
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/"
+                className="flex gap-1 items-center text-[#1C1C1C] font-medium"
+              >
+                Help <MdExpandMore className="text-[#8B96A5]" size="22" />
+              </Link>
+            </li>
+          </ul>
+          <ul className="flex items-center gap-9">
+            <li>
+              <Link
+                href="/"
+                className="flex gap-1 items-center text-[#1C1C1C] font-medium"
+              >
+                English, USD{" "}
+                <MdExpandMore className="text-[#8B96A5]" size="22" />
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/"
+                className="flex gap-1 items-center text-[#1C1C1C] font-medium"
+              >
+                Ship to{" "}
+                <ReactCountryFlag
+                  countryCode="US"
+                  svg
+                  style={{
+                    width: "1em",
+                    height: "1em",
+                  }}
+                  title="US"
+                />
+                <MdExpandMore className="text-[#8B96A5]" size="22" />
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </Container>
+    </nav>
+  );
+}
